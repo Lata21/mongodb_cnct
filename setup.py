0cff118900fb03f1d44763e0f5b339392fd3a245
Lata21 <latab2130@gmail.com>
2024-01-02 22:38:19 +0530

    code version updated

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.6"
+__version__ = "0.0.4"
 REPO_NAME = "database_automation"
 PKG_NAME= "MongoDbConnect"
 AUTHOR_USER_NAME = "Lata21"
